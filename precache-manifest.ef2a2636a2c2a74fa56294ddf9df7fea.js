8bc4ac4d5d41eab92b84385fa7fcfbc96cebe528
piush-terminalwitchcraft <piushpaul.16@gmail.com>
2022-06-27 15:55:11 +0530

    Updates

---
+++ precache-manifest.ef2a2636a2c2a74fa56294ddf9df7fea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "aacc2cb1186004091365",
+    "url": "/ProctoEase/static/css/main.ce4edd13.chunk.css"
+  },
+  {
+    "revision": "aacc2cb1186004091365",
+    "url": "/ProctoEase/static/js/main.aacc2cb1.chunk.js"
+  },
+  {
+    "revision": "e09d301d0b2ba1925eb7",
+    "url": "/ProctoEase/static/js/1.e09d301d.chunk.js"
+  },
+  {
+    "revision": "f82fd86bc6cca94beca3",
+    "url": "/ProctoEase/static/js/runtime~main.f82fd86b.js"
+  },
+  {
+    "revision": "e74d1a59c38abfc99efd879de81be87e",
+    "url": "/ProctoEase/static/media/logofont.e74d1a59.svg"
+  },
+  {
+    "revision": "eb15948586c34f6d94523c744f789386",
+    "url": "/ProctoEase/static/media/logo_procto_ease_no_background.eb159485.png"
+  },
+  {
+    "revision": "7cb1b527ebccae09d0990d2cadfdb2b9",
+    "url": "/ProctoEase/static/media/infinite.7cb1b527.svg"
+  },
+  {
+    "revision": "e74f1927b638561085eec5310acfcfdb",
+    "url": "/ProctoEase/static/media/human.e74f1927.svg"
+  },
+  {
+    "revision": "1d06066e1ed55e8ce343338af7052084",
+    "url": "/ProctoEase/index.html"
+  }
+];
